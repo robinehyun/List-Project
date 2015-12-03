9c4c07c871415836ea038fac5cf78875a044fce3
Robin Yun <robinehyun@gmail.com>
2015-12-02 21:22:31 -0800

    Fixed remove target

--- js/event-attributes.js
+++ js/event-attributes.js
@@ -1,37 +1,4 @@
-// function checkUsername() {
-//   var elMsg = document.getElementById('feedback');
-//   if (this.value.length < 5) {
-//       elMsg.textContent = 'Username must be 5 characters or more';
-//     } else {
-//       elMsg.textContent = '';
-//     }
-//   }
-//
-// var elUsername = document.getElementById('username');
-// // elUsername.onblur = checkUsername;
-// elUsername.addEventListener('blur', checkUsername, false);
 
-// var elUsername = document.getElementById('username');
-// var elMsg = document.getElementById('feedback');
-//
-// function checkUsername(minLength) {
-//   if (elUsername.value.length < minLength) {
-//     elMsg.textContent = 'Username must be ' + minLength + ' characters or more';
-//   } else {
-//     elMsg.innerHTML = '';
-//   }
-// }
-//
-// if (elUsername.addEventListener) {
-// elUsername.addEventListener('blur', function(){
-//   checkUsername(5);
-// }, false);
-// } else {
-//   //IE5-8 Fallback
-//   elUsername.attachEvent('blur',function(){
-//     checkUsername(5);
-//   }, false);
-// }
 
 function checkUsername() {
   //Store username variable
@@ -58,62 +25,14 @@ var elMsg = document.getElementById('feedback');
 
 el.addEventListener('focus', tipUsername, false);
 console.log(el);
-elMsg.addEventListener('blur', checkUsername, false);
+el.addEventListener('blur', checkUsername, false);
 
-//declare function
-// function checkLength(e, minLength) {
-//   //declare variables
-//   var el, elMsg;
-//   //If event object doesnt exist
-//   if (!e) {
-//     //Use IE fallback
-//     e = window.event;
-//   }
-//   //Get target of event. srcElement is target property for IE5-8 equivalent
-//   el = e.target || e.srcElement;
-//   //Get its next sibling
-//   elMsg = el.nextSibling;
-//   //If length is too short set msg
-//   if (el.value.length < minLength) {
-//     elMsg.innerHTML = 'Username must be ' + minLength + ' characters of more';
-//   } else {
-//     elMsg.innerHTML = '';
-//   }
-// }
-// //Get username input
-// var elUsername = document.getElementById('username');
-// //If event listener supported
-// if (elUsername.addEventListener) {
-//   //on blur event
-//   elUsername.addEventListener('blur', function(e){
-//     //call checklength
-//     checkLength(e, 5);
-//     //capture in bubble phase
-//   }, false);
-//   //Otherwise
-// } else {
-//   //IE fallback on blur
-//   elUsername.attachEvent('blur', function(e){
-//     checkLength(e,5);
-//   }, false);
-// }
 
 var elPassword = document.getElementById('password');
 var elMsg2 = document.getElementById('feedback2');
 
 function checkPassword(minLength) {
-//   if (str.length < 6) {
-//     elMsg2.textContent = 'Password must be 6 characters or more';
-// } else if (str.length > 50) {
-//     elMsg2.textContent = 'Password must be 50 characters or less';
-// } else if (str.search(/\d/) == -1) {
-//     elMsg2.textContent = 'Password must contain 1 number';
-// } else if (str.search(/[a-zA-Z]/) == -1) {
-//     elMsg2.textContent = 'Password must contain 1 letter';
-// } else if (str.search(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+]/) != -1) {
-//     elMsg2.textContent = 'Password must must contain a special character: !@#$%^&*()_+';
-// }
-// return("ok");
+
   if (elPassword.value.length < minLength) {
     elMsg2.textContent = 'Password must be ' + minLength + ' characters or more';
   } else {
--- js/list.js
+++ js/list.js
@@ -1,104 +1,35 @@
-// var el = document.getElementById('one');
-//
-// el.className = 'cool';
-
-// var elements = document.getElementsByClassName('hot');
-//
-// if(elements.length > 2) {
-//   var el = elements[2];
-//   el.className = 'cool';
-// }
-
-// var hotItems = document.querySelectorAll('li.hot');
-//
-// if (hotItems.length > 0) {
-//   for(var i = 0; i < hotItems.length; i++) {
-//     hotItems[i].className = 'cool';
-//   }
-// };
-
-// var startItem = document.getElementById('two');
-// var prevItem = startItem.previousSibling;
-// var nextItem = startItem.nextSibling;
-// console.log(prevItem);
-// prevItem.className = 'complete';
-// nextItem.className = 'cool';
-//
-// var startItem = document.getElementsByTagName('ul')[0];
-// var firstItem = startItem.firstChild;
-// var lastItem = startItem.lastChild;
-//
-// firstItem.setAttribute('class', 'complete');
-// lastItem.setAttribute('class', 'cool');
-
-// var itemTwo = document.getElementById('two');
-// var elText = itemTwo.firstChild.nodeValue;
-// elText = elText.replace('pine nuts', 'kale');
-// itemTwo.firstChild.nodeValue = elText;
-
-// var listItem = document.createElement('li');
-// var textItem = document.createTextNode('quinoa');
-//
-// listItem.appendChild(textItem);
-//
-// var position = document.getElementsByTagName('ul')[0];
-//
-// position.appendChild(listItem);
-//
-// listItem.setAttribute('id', 'four');
-
-function getTarget(e) {
-  if (!e) {
-    e = window.event;
-  }
-  return e.target || e.srcElement;
-}
-
-function itemDone(e) {
-  var target, elParent, elGrandparent;
-  target = getTarget(e);
-  elParent = target.parentNode;
-  elGrandparent = target.parentNode.parentNode;
-  elGrandparent.removeChild(elParent);
-
-  if (e.preventDefault) {
-    e.preventDefault();
-  } else {
-    e.returnValue = false;
+function itemDone(ev) {
+  var target = ev.target;
+  console.log(target);
+  if(ev.target.tagName.toLowerCase() === 'a') {
+    ev.preventDefault();
+    target.parentNode.remove();
   }
 }
 
-var el = document.getElementById('shoppingList');
-if (el.addEventListener) {
-  el.addEventListener('click', function(e) {
+var shoppingListContainer = document.getElementById('shoppingList');
+if (shoppingListContainer.addEventListener) {
+  shoppingListContainer.addEventListener('click', function(e) {
     itemDone(e);
   }, false);
 } else {
-  el.attachEvent('click', function(e) {
+  shoppingListContainer.attachEvent('click', function(e) {
     itemDone(e);
   }, false);
 }
 
-// function addList() {
-//
-// var addForm = document.querySelectorAll('.add');
-// var addItem = document.getElementsByClassName('add-form');
-//
-// addForm[0].addEventListener('click', function() {
-//   addItem.classList.remove('.close');
-//   addItem.classList.add('.open');
-//   addItem.querySelectorAll('input')[0].focus();
-// });
-// }
-//
-// addList();
 
-var button = document.getElementsByClassName('buttonAdd');
-var form = document.getElementsByClassName('add-form');
 
-button.onClick = function() {
-  form.classList.remove('close');
-  console.log(form);
-  form.classList.add('open');
-  form.querySelectorAll('input')[0].focus();
-};
+var button = document.querySelector('.buttonAdd');
+var form = document.querySelector('.add-form');
+
+
+var addItem = function(ev) {
+console.log(form);
+ form.classList.remove('close');
+ form.classList.add('open');
+ form.querySelectorAll('input')[0].focus();
+ };
+
+console.log(button);
+button.addEventListener('click', addItem);
